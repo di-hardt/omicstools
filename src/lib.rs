f995c6991db942d564d8233f7316e6a10c842402
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2023-08-02 16:56:38 +0200

    add crate information and license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// # Create absolute path to readme ti increase compatible for different build targets
+//  https://gist.github.com/JakeHartnell/2c1fa387f185f5dc46c9429470a2e2be
+#![doc = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/Readme.md"))]
+
 /// Module containing functions and information for chemistry and molecules
 pub mod chemistry;
 /// Module containing functions and definitions for proteomics
